7c8f8cfa35a3952076f7f356afbadb94073352f8
himself65 <himself6565@gmail.com>
2019-03-30 21:41:09 +0800

    test: `FileManager`
    Signed-off-by: himself65 <himself6565@gmail.com>

--- packages/rrreol-core/src/fileManager.js
+++ packages/rrreol-core/src/fileManager.js
@@ -1,26 +1,36 @@
 import chalk from 'chalk'
 import fs from 'fs-extra'
-import { isNil } from 'ramda'
+import { empty, join, isNil } from 'ramda'
 
 export class FileManager {
   constructor (path) {
+    this.__content = null
+    this.filePath = path || null
+
     if (fs.existsSync(path)) {
       this.__content = fs.readFileSync(path)
         .toString()
         .split('\n')
     }
-    this.filePath = path || ''
   }
 
   static save = async (path, content) => {
     await fs.writeFile(path, content)
   }
 
+  content = () => {
+    if (isNil(this.__content)) {
+      return empty('')
+    } else {
+      return join('\n')(this.__content)
+    }
+  }
+
   save = async (path) => {
     if (!isNil(path)) {
-      await FileManager.save(path, this.__content)
+      await FileManager.save(path, this.content())
     } else {
-      await fs.writeFile(this.filePath, this.__content)
+      await fs.writeFile(this.filePath, this.content())
     }
   }
 
--- packages/rrreol-core/test/unit/fileManager.test.js
+++ packages/rrreol-core/test/unit/fileManager.test.js
@@ -1,27 +1,57 @@
-import { FileManager } from '@/fileManager'
 import { resolve } from 'path'
+import { existsSync, readFileSync } from 'fs'
+import { FileManager } from '@/fileManager'
+import { removeFiles } from '@/utils'
 
 describe('FileManager base test', () => {
   it('should init success', () => {
     const fileManager = new FileManager()
     expect(fileManager).toBeDefined()
+    expect(fileManager.__content).toBeNull()
+    expect(fileManager.filePath).toBeNull()
+    expect(fileManager.content()).toEqual('')
+  })
+})
+
+describe('FileManager File System test', () => {
+  const fixturesPath = resolve(__dirname, '..', 'fixtures')
+  const inputFilePath = resolve(fixturesPath, 'base.out')
+  const outFilePath = resolve(fixturesPath, '1.out.test.out')
+
+  let fileManager = null
+
+  beforeEach(() => {
+    fileManager = new FileManager()
+  })
+
+  it('should exist files', () => {
+    expect(existsSync(fixturesPath)).toBe(true)
+    expect(existsSync(inputFilePath)).toBe(true)
   })
 
   it('should have correct type', async () => {
-    const filePath = resolve(__dirname, '..', 'fixtures', 'base.out')
-
-    let fileManager = new FileManager()
-    await fileManager.read(filePath)
+    await fileManager.read(inputFilePath)
     expect(fileManager.__content).toBeInstanceOf(Array)
 
-    fileManager = new FileManager(filePath)
+    fileManager = new FileManager(inputFilePath)
     expect(fileManager.__content).toBeInstanceOf(Array)
   })
 
   it('should read file correctly', async () => {
-    const filePath = resolve(__dirname, '..', 'fixtures', 'base.out')
-    const fileManager = new FileManager()
-    await fileManager.read(filePath)
+    await fileManager.read(inputFilePath)
     expect(fileManager.lines()).toEqual(3)
+  })
+
+  it('should save file correctly', async () => {
+    await fileManager.read(inputFilePath)
+    expect(fileManager.content()).toEqual('1\n2\n')
+    await fileManager.save(outFilePath)
+    expect(existsSync(outFilePath)).toBe(true)
+    const content = readFileSync(outFilePath).toString()
+    expect(content).toEqual('1\n2\n')
+  })
+
+  afterAll(() => {
+    removeFiles(fixturesPath, /\.test\.out$/)
   })
 })

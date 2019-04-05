d8eb5f29edb77086b44615f2f05a8d0713615190
himself65 <himself6565@gmail.com>
2019-04-05 19:21:30 +0800

    feat: add build config on `core`
    Signed-off-by: himself65 <himself6565@gmail.com>

--- packages/rrreol-core/src/compiler.js
+++ packages/rrreol-core/src/compiler.js
@@ -1,4 +1,4 @@
-import * as cp from 'child_process'
+import { spawn } from 'child_process'
 import { isString } from 'lodash'
 import { empty, isNil, curry } from 'ramda'
 
@@ -11,7 +11,7 @@ Compiler.compile = curry(async (path, output) => {
     return Promise.reject(new TypeError('path or output is not string'))
   }
   // fixme: add more command options
-  const childProcess = cp.spawn('g++', ['-o', output, path])
+  const childProcess = spawn('g++', ['-o', output, path])
   return new Promise((resolve, reject) => {
     childProcess.on('exit', (code) => {
       if (code !== 0) {

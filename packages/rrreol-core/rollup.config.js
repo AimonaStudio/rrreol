4cffd15d0152a0012637f3c894a2dfe4c0d1374b
扩散性百万甜面包 <Himself6565@gmail.com>
2019-04-05 21:04:20 +0800

    feat: add build config on `core` (#88)
    Signed-off-by: himself65 <himself6565@gmail.com>

---
+++ packages/rrreol-core/rollup.config.js
@@ -0,0 +1,38 @@
+import packageJson from './package.json'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default {
+  input: packageJson.source,
+  output: {
+    file: packageJson.main,
+    format: 'cjs'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: [
+        '@babel/external-helpers'
+      ],
+      externalHelpers: true
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      modulesOnly: true
+    }),
+    commonjs({
+      exclude: 'src/**',
+      include: 'node_modules/**'
+    }),
+    isProd && terser()
+  ],
+  external: id => [
+    /ramda/,
+    /lodash/
+  ].some(regex => regex.test(id))
+}
